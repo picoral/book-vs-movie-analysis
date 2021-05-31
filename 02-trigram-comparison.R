3f739f0d039cf7dec3440c98bb3f04f633f88c0b
picoral <adrianaps@arizona.edu>
2021-05-31 09:56:32 -0700

    worked on the trigram comparsion script

--- 02-trigram-comparison.R
+++ 02-trigram-comparison.R
@@ -10,3 +10,19 @@ emma_script <- read_tsv("processed-data/emma-script.tsv") %>%
 
 # create a mutate to add a column called "paragraph" to both
 # data frames that contain paragraph number # 45 min after the hours
+emma_book <- emma_book %>%
+  mutate(paragraph = rownames(emma_book))
+
+emma_script <- emma_script %>%
+  mutate(paragraph = rownames(emma_script))
+
+# combine both corpora into one dataframe
+emma_corpora <- bind_rows(emma_book,
+                          emma_script) %>%
+  rename(text = ".")
+
+# count how many paragraphs per corpora
+emma_corpora %>%
+  count(source)
+
+
